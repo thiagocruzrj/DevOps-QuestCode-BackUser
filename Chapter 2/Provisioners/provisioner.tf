6bd0acf6183062bba925725221fde6cba8ebe497
Thiago Cruz <thagocruz@gmail.com>
2022-01-13 22:36:02 -0300

    executing base provisioner

--- Chapter 2/Provisioners/provisioner.tf
+++ Chapter 2/Provisioners/provisioner.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "powershellprocess" {
 
   provisioner "local-exec" {
-    command = "Get-Process > list.txt"
+    command     = "Get-Process > list.txt"
     interpreter = ["Powershell", "-command"]
   }
 } 
