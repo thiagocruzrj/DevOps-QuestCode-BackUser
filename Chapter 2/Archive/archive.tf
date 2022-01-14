dd194e3b1e031e0cf53a6536e38d41f1c128382b
Thiago Cruz <thagocruz@gmail.com>
2022-01-13 22:20:55 -0300

    formating with terraform fmt

--- Chapter 2/Archive/archive.tf
+++ Chapter 2/Archive/archive.tf
@@ -1,17 +1,17 @@
 terraform {
-    required_providers {
-      archive = {
-          source = "hashicorp/archive"
-      }
+  required_providers {
+    archive = {
+      source = "hashicorp/archive"
     }
+  }
 }
 
 data "archive_file" "zipfile" {
-    type = "zip"
-    source_dir = "data_backup"
-    output_path = "backup_dir.zip"
+  type        = "zip"
+  source_dir  = "data_backup"
+  output_path = "backup_dir.zip"
 }
 
 output "fileZip" {
-    value = data.archive_file.zipfile.output_size
+  value = data.archive_file.zipfile.output_size
 }
