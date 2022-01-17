9c8e7f022a25c56ebc9327e5dfbe69fe78f4344e
Thiago Cruz <thagocruz@gmail.com>
2022-01-17 17:41:31 -0300

    Create main.tf

---
+++ Chapter 5/main.tf
@@ -0,0 +1,13 @@
+provider "azurerm"{
+    features{
+
+    }
+}
+
+resource "azurerm_resource_group" "group-resource-chapter5"{
+    location = "brazilsouth"
+    name = "rg-terraform-chp5"
+    tags = {
+        data = formatdate("DD MM YYYY hh:mm ZZZ", timestamp())
+    }
+}
