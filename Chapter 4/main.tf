ebd7f0b70ff6ea3d3b9376d32fab7982de6bbebc
Thiago Cruz <thagocruz@gmail.com>
2022-01-17 15:48:26 -0300

    merging files

--- Chapter 4/main.tf
+++ Chapter 4/main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
 resource "azurerm_resource_group" "resource-group" {
     name = var.name-rg
     location = var.location
-    tags = var.tags
+    tags = merge(var.tags, { course = "terraform" })
 }
 
 resource "azurerm_virtual_network" "vnet" {
