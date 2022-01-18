a43097ea443def607649e3ec1552a40de56f8da3
Thiago Cruz <thagocruz@gmail.com>
2022-01-18 12:46:32 -0300

    Provisionating storage account

--- Chapter 6/Blobstorage/main.tf
+++ Chapter 6/Blobstorage/main.tf
@@ -16,6 +16,47 @@ provider "random"{
 
 provider "azurerm"{
     features {
-        
+
     }
+}
+
+resource "azurerm_resource_group" "rgstorage"{
+    name = "storagerg"
+    location = "brazilsouth"
+}
+
+resource "random_string" "random"{
+    length = 3
+    special = false
+    upper = false
+    number = true
+}
+
+resource "azurerm_storage_account" "storagetf" {
+    name = "tftraining${random_string.random.result}"
+    resource_group_name = "storagerg"
+    location = "brazilsouth"
+    account_tier = "Standard"
+    access_tier = "Hot"
+    account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container" {
+    name = "terraform"
+    storage_account_name = azurerm_storage_account.storagetf.name
+    depends_on = [ azurerm_storage_account.storagetf ]
+}
+
+output "blobstorage-name" {
+    value = azurerm_storage_account.storagetf.name
+}
+
+output "blobstorage-primary-key" {
+    value = azurerm_storage_account.storagetf.primary_access_key
+    sensitive = true
+}
+
+output "blobstorage-secondary-key" {
+    value = azurerm_storage_account.storagetf.secondary_access_key
+    sensitive = true
 }
