aea166f35731f3d4da7d3ba7f21141457e175cfc
Thiago Cruz <thagocruz@gmail.com>
2022-01-18 14:45:49 -0300

    Update main.tf

--- Chapter 7/Folder1/main.tf
+++ Chapter 7/Folder1/main.tf
@@ -11,3 +11,32 @@ resource "azurerm_resource_group" "rg" {
   name = "appservice-rg"
   location = var.location
 }
+
+resource "azurerm_app_service_plan" "plan" {
+  name = "azureserviceplan"
+  location = var.location
+  resource_group_name = azure_resource_group.rg.name
+  sku {
+      tier = "Standard"
+      size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "appservice"{
+    name = "tfappservicechp7"
+    location = var.location
+    resoucer_group_name = azurerm_resource_group.rg.name
+    app_service_plan_id = azurerm_app_service_plan.plan.id
+
+    site_config{
+        dotnet_framework_version = "v4.0"
+    }
+
+    app_settings = {
+        "key" = "123456"
+    }
+
+    output "url" {
+        value = azurerm_app_service.appservice.default_site_hostname
+    }
+}
