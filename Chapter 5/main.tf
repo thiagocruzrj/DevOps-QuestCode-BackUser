c1a03acf025f5b37075e7402d42227c48b30dc54
Thiago Cruz <thagocruz@gmail.com>
2022-01-17 21:25:14 -0300

    using concat

--- Chapter 5/main.tf
+++ Chapter 5/main.tf
@@ -14,3 +14,15 @@ resource "azurerm_resource_group" "group-resource-chapter5"{
         technology = title("terraform")
     }
 }
+
+variable "vnetips" {
+    type = list
+    default = ["10.0.0.0/16"]
+}
+
+resource "azurerm_virtual_network" "vnet"{
+    name = "vnetazuretraining"
+    location = "brazilsouth"
+    resource_group_name = "rg-terraform-chp5"
+    address_space = concat(var.vnetips, [ "192.168.0.0/16" ])
+}
