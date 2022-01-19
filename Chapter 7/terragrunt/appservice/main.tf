3e5ff74452840ef67275d12acedb7d8ae3d450b3
Thiago Cruz <thagocruz@gmail.com>
2022-01-18 21:48:09 -0300

    Create main.tf

---
+++ Chapter 7/terragrunt/appservice/main.tf
@@ -0,0 +1,30 @@
+resource "azurerm_resource_group" "rg"{
+    name = var.rg
+    location = var.location
+}
+
+resource "azurerm_app_service_plan" "plan"{
+    name = "appserviceplantf"
+    location = var.location
+    resource_group_name = var.rg
+
+    sku {
+        tier = "Standard"
+        size = "S1"
+    }
+}
+
+resource "azurerm_app_service" "appservice"{
+    name = "azappservicetraining"
+    resource_group_name = var.rg
+    location = var.location
+    app_service_plan_id = azurerm_app_service_plan.plan.id
+
+    site_config{
+        dotnet_framework_version = "v4.0"
+    }
+
+    app_settings = {
+        key = "123456"
+    }
+}
