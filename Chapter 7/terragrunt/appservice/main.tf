f873e8847710649a6d8db297cda18da82d3ac91c
Thiago Cruz <thagocruz@gmail.com>
2022-01-19 09:31:57 -0300

    Update main.tf

--- Chapter 7/terragrunt/appservice/main.tf
+++ Chapter 7/terragrunt/appservice/main.tf
@@ -1,30 +1,30 @@
-resource "azurerm_resource_group" "rg"{
-    name = var.rg
-    location = var.location
+resource "azurerm_resource_group" "rg" {
+  name = var.rg
+  location = var.location
 }
 
-resource "azurerm_app_service_plan" "plan"{
-    name = "appserviceplantf"
-    location = var.location
-    resource_group_name = var.rg
+resource "azurerm_app_service_plan" "plan" {
+  name = "appserviceplantf"
+  location = var.location
+  resource_group_name = var.rg
 
     sku {
-        tier = "Standard"
-        size = "S1"
+      tier = "Standard"
+      size = "S1" 
     }
 }
 
-resource "azurerm_app_service" "appservice"{
-    name = "azappservicetraining"
+resource "azurerm_app_service" "appservice" {
+    name = "azapptftreinamento"
     resource_group_name = var.rg
     location = var.location
     app_service_plan_id = azurerm_app_service_plan.plan.id
 
-    site_config{
-        dotnet_framework_version = "v4.0"
+    site_config {
+      dotnet_framework_version = "v4.0"
     }
-
+    
     app_settings = {
-        key = "123456"
+      "chave" = "654321"
     }
 }
