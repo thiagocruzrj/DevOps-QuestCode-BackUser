81acf2c15e9a2bbde37426dbd75875a57810a2a9
Thiago Cruz <thagocruz@gmail.com>
2022-01-20 10:03:39 -0300

    Update main.tf

--- Chapter 10/pipelines/main.tf
+++ Chapter 10/pipelines/main.tf
@@ -1,5 +1,7 @@
 terraform {
-  backend "azurerm"{ }
+  backend "azurerm"{
+
+   }
 }
 
 provider "azurerm"{
@@ -9,13 +11,13 @@ provider "azurerm"{
 resource "azurerm_resource_group" "rg"{
     name = "rg-terraform-pipeline-${terraform.workspace}"
     location = var.location
-    tags = var.tags
+    tags = merge(var.tags, {"workspace" = "${terraform.workspace}"})
 }
 
 resource "azurerm_app_service_plan" "plan"{
     name = "appplan-azpipeline-${terraform.workspace}"
     location = var.location
-    tags = var.tags
+    tags = merge(var.tags, {"workspace" = "${terraform.workspace}"})
     resource_group_name = azurerm_resource_group.rg.name
     sku {
         size = "F1"
@@ -26,7 +28,7 @@ resource "azurerm_app_service_plan" "plan"{
 resource "azurerm_app_service" "appservice" {
     name = "azpipeline-tftreinamento-${terraform.workspace}"
     location = var.location
-    tags = var.tags
+    tags = merge(var.tags, {"workspace" = "${terraform.workspace}"})
     resource_group_name = azurerm_resource_group.rg.name
     app_service_plan_id = azurerm_app_service_plan.plan.id
 }
