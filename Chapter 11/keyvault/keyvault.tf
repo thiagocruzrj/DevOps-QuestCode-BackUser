95343d5b41a75d3b96ce4ba93c996d076962071d
Thiago Cruz <thagocruz@gmail.com>
2022-01-20 10:38:52 -0300

    Update keyvault.tf

--- Chapter 11/keyvault/keyvault.tf
+++ Chapter 11/keyvault/keyvault.tf
@@ -1,8 +1,25 @@
 provider "azurerm" {
-    features {}
+  features {}
 }
 
-resource "azurerm_resource_group" "rg"{
-    name = "rg-keyvault"
-    location = "brazilsouth"
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-keyvault"
+  location = "brazilsouth"
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "keyvault" {
+  name                       = "keyvaulttf"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "premium"
+  soft_delete_retention_days = 7
+  access_policy {
+    key_permissions    = ["create", "get"]
+    object_id          = data.azurerm_client_config.current.object_id
+    tenant_id          = data.azurerm_client_config.current.tenant_id
+    secret_permissions = ["set", "get", "delete", "purge", "recover"]
+  }
 }
