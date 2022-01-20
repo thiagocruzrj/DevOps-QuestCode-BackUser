35258826d51d63101d10a05801e1e4c21c85f612
Thiago Cruz <thagocruz@gmail.com>
2022-01-19 21:56:26 -0300

    Create main.tf

---
+++ Chapter 10/pipelines/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  backend "azurerm"{ }
+}
+
+provider "azurerm"{
+    features{}
+}
+
+resource "azurerm_resource_group" "rg"{
+    name = "rg-terraform-pipeline"
+    location = var.location
+    tags = var.tags
+}
+
+resource "azurerm_app_service_plan" "plan"{
+    name = "appplan-azpipeline"
+    location = var.location
+    tags = var.tags
+    resource_group_name = azurerm_resource_group.rg.name
+    sku {
+        size = "F1"
+        tier = "Standard"
+    }
+}
+
+resource "azurerm_app_service" "appservice" {
+    name = "azpipeline-tftreinamento"
+    location = var.location
+    tags = var.tags
+    resource_group_name = azurerm_resource_group.rg.name
+    app_service_plan_id = azurerm_app_service_plan.plan.id
+}
