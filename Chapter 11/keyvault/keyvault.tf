d25b61d91b1f3e4a80d81adcd0bed51a21db11e7
Thiago Cruz <thagocruz@gmail.com>
2022-01-20 13:40:38 -0300

    Update keyvault.tf

--- Chapter 11/keyvault/keyvault.tf
+++ Chapter 11/keyvault/keyvault.tf
@@ -10,14 +10,14 @@ resource "azurerm_resource_group" "rg" {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "keyvault" {
-  name                       = "keyvaulttf"
+  name                       = "keyvaulttfrj"
   location                   = azurerm_resource_group.rg.location
   resource_group_name        = azurerm_resource_group.rg.name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "premium"
   soft_delete_retention_days = 7
   access_policy {
-    key_permissions    = ["create", "get"]
+    key_permissions    = ["create", "get", "list"]
     object_id          = data.azurerm_client_config.current.object_id
     tenant_id          = data.azurerm_client_config.current.tenant_id
     secret_permissions = ["set", "get", "delete", "purge", "recover"]
