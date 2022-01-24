24f1efca08a1bb043b7c0da57bbb3ea57c736946
Thiago Cruz <thagocruz@gmail.com>
2022-01-24 20:28:25 -0300

    allow azure db resourcers

--- Chapter 14/AzDatabase/firewall.tf
+++ Chapter 14/AzDatabase/firewall.tf
@@ -1,5 +1,5 @@
 data "http" "ip_address" {
-  url = "https://api.apify.org"
+  url = "https://api.ipify.org"
   request_headers = {
     Accept = "text/plain"
   }
@@ -11,4 +11,12 @@ resource "azurerm_sql_firewall_rule" "sqlfirewall-myip" {
   server_name         = azurerm_sql_server.sqlserver.name
   start_ip_address    = data.http.ip_address.body
   end_ip_address      = data.http.ip_address.body
+}
+
+resource "azurerm_sql_firewall_rule" "sqlfirewall-azureservices" {
+    name = "AllowAzureServices"
+    resource_group_name = azurerm_resource_group.rg.name
+    server_name         = azurerm_sql_server.sqlserver.name
+    start_ip_address    = "0.0.0.0"
+    end_ip_address      = "0.0.0.0"
 }
