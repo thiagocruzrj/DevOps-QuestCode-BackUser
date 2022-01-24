ff922363d2551dc99a0cac4a1b1334000fb3999d
Thiago Cruz <thagocruz@gmail.com>
2022-01-24 16:10:59 -0300

    add sql database

--- Chapter 14/AzDatabase/sqdatabase.tf
+++ Chapter 14/AzDatabase/sqdatabase.tf
@@ -17,10 +17,17 @@ resource "azurerm_sql_server" "sqlserver" {
   administrator_login_password = "thisisNotMyP@$$word"
 }
 
-resource "azurerm_mssql_database" "mssqldatabase"{
-    name = "db_terraform"
-    server_id = azurerm_sql_server.sqlserver.id
-    collation = "SQL_Latin1_General_CP1_CI_AS"
-    license_type = "LicenseIncluded"
-    sku_name = "Basic"
+resource "azurerm_mssql_database" "mssqldatabase" {
+  name         = "db_terraform"
+  server_id    = azurerm_sql_server.sqlserver.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  sku_name     = "Basic"
+}
+
+resource "azurerm_sql_database" "azsqldatabase" {
+  name                = "azsqldatabase-terraform"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  server_name         = azurerm_sql_server.sqlserver.name
 }
