a402abfe53bcfe5ca30c216b38fc316a1d082ce5
Thiago Cruz <thagocruz@gmail.com>
2022-01-24 22:19:00 -0300

    Add postegress database and rules

--- Chapter 14/PostgreSql/postgresql.tf
+++ Chapter 14/PostgreSql/postgresql.tf
@@ -26,3 +26,34 @@ resource "azurerm_postgresql_server" "postgreserver" {
   public_network_access_enabled     = true
   auto_grow_enabled                 = true
 }
+
+resource "azurerm_postgresql_database" "postgredb" {
+  name                = "postgredb-terraform"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_postgresql_server.postgreserver.name
+
+  charset   = "UTF8"
+  collation = "en_US"
+}
+
+data "http" "ip_address" {
+  url = "https://api.ipify.org"
+  request_headers = {
+    Accept = "text/plain"
+  }
+}
+resource "azurerm_postgresql_firewall_rule" "myip" {
+  name                = "personalip"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_postgresql_server.postgreserver.name
+  start_ip_address    = data.http.ip_address.body
+  end_ip_address      = data.http.ip_address.body
+}
+
+resource "azurerm_postgresql_firewall_rule" "allow-azservices" {
+  name                = "allow-azure-services"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_postgresql_server.postgreserver.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
