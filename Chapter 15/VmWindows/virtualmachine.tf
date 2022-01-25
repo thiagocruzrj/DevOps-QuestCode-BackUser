afb5c8b52293024b87a4f67964a29b2290c0c691
Thiago Cruz <thagocruz@gmail.com>
2022-01-25 11:11:33 -0300

    Add nic

--- Chapter 15/VmWindows/virtualmachine.tf
+++ Chapter 15/VmWindows/virtualmachine.tf
@@ -29,3 +29,16 @@ resource "azurerm_public_ip" "publicip" {
   idle_timeout_in_minutes = 30
   domain_name_label       = "vmwinservertf"
 }
+
+resource "azurerm_network_interface" "nic" {
+  name                = "vmwinserver-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "externalip-config"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+}
