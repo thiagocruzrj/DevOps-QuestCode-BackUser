5d41d345390734c14916d2ada7de7ec0f84527b6
Thiago Cruz <thagocruz@gmail.com>
2022-01-24 21:19:01 -0300

    Add firewall rules

--- Chapter 14/MariaDb/mariadb.tf
+++ Chapter 14/MariaDb/mariadb.tf
@@ -30,3 +30,25 @@ resource "azurerm_mariadb_database" "mariadb" {
   charset             = "utf8"
   collation           = "utf8_general_ci"
 }
+
+data "http" "ip_address" {
+  url = "https://api.ipify.org"
+  request_headers = {
+    Accept = "text/plain"
+  }
+}
+resource "azurerm_mariadb_firewall_rule" "myip" {
+  name                = "personalip"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_mariadb_server.mariadbserver.name
+  start_ip_address    = data.http.ip_address.body
+  end_ip_address      = data.http.ip_address.body
+}
+
+resource "azurerm_mariadb_firewall_rule" "allow-azservices" {
+  name                = "allow-azure-services"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_mariadb_server.mariadbserver.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
