1c5128c2d6991a2103f7982b4e1554180ab5b560
Thiago Cruz <thagocruz@gmail.com>
2022-01-25 11:04:21 -0300

    Add subnet

--- Chapter 15/VmWindows/virtualmachine.tf
+++ Chapter 15/VmWindows/virtualmachine.tf
@@ -13,3 +13,10 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "vmwinserver-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
