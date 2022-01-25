ab05366391f5433cd336d25bfb90568bfd6f9449
Thiago Cruz <thagocruz@gmail.com>
2022-01-25 11:07:43 -0300

    Add public ip

--- Chapter 15/VmWindows/virtualmachine.tf
+++ Chapter 15/VmWindows/virtualmachine.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vminserver-vnet"
+  name                = "vmwinserver-vnet"
   address_space       = ["10.0.0.0/16", "192.168.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -19,4 +19,13 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "publicip" {
+  name                    = "vmwinserver-publicip"
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  allocation_method       = "Dynamic"
+  idle_timeout_in_minutes = 30
+  domain_name_label       = "vmwinservertf"
 }
